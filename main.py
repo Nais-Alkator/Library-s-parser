8ecb26b0446866fed067ce4c53155fd4586d705a
Alkator-Nori <nais000k@gmail.com>
2020-06-01 21:24:01 +0300

    get image's links

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import os
 from pathvalidate import sanitize_filename
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 
 os.makedirs("books", exist_ok=True)
@@ -14,15 +15,19 @@ def download_books(folder="books"):
         url_for_title = "http://tululu.org/b{}".format(book)
         response_for_title = requests.get(url_for_title)
         response_for_title.raise_for_status()
-        soup = BeautifulSoup(response_for_title.text, 'lxml')
-        title = soup.find("h1")
-        title = title.text.split("::")
-        header = title[0].strip()
-        header = "{}. ".format(book) + header
-        header = sanitize_filename(header)
-        filename = os.path.join(folder, header)
-
         if response_for_download.content:
-            with open(filename, 'wb') as file:
-                file.write(response_for_download.content)
+            soup = BeautifulSoup(response_for_title.text, 'lxml')
+            title = soup.find("h1")
+            title = title.text.split("::")
+            header = title[0].strip()
+            header = "{}. ".format(book) + header
+            header = sanitize_filename(header)
+            image = soup.find("div", class_="bookimage").find('a').find("img")["src"]
+            image = urljoin(url_for_title, image)
+            filename = os.path.join(folder, header)
+            print("Заголовок:", header)
+            print("Картинка:", image)
+       # if response_for_download.content:
+            #with open(filename, 'wb') as file:
+                #file.write(response_for_download.content)
 download_books()
