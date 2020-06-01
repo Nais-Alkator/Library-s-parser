72ccdcc35a65445d6638361262acf822569489da
Alkator-Nori <nais000k@gmail.com>
2020-06-01 23:31:04 +0300

    download book covers

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 
-os.makedirs("books", exist_ok=True)
+books_folder = os.makedirs("books", exist_ok=True)
+images_folder = os.makedirs("images", exist_ok=True)
+
 
 def download_books(folder="books"):
     for book in range(1, 11):
@@ -22,12 +24,32 @@ def download_books(folder="books"):
             header = title[0].strip()
             header = "{}. ".format(book) + header
             header = sanitize_filename(header)
-            image = soup.find("div", class_="bookimage").find('a').find("img")["src"]
+            image = soup.find("div", class_="bookimage").find("a").find("img")["src"]
             image = urljoin(url_for_title, image)
             filename = os.path.join(folder, header)
-            print("Заголовок:", header)
-            print("Картинка:", image)
-       # if response_for_download.content:
-            #with open(filename, 'wb') as file:
-                #file.write(response_for_download.content)
-download_books()
+
+        if response_for_download.content:
+            with open(filename, 'wb') as file:
+                file.write(response_for_download.content)
+
+
+def download_image(images_folder="images"):
+    for book in range(1, 11):
+        url_for_download = "http://tululu.org/txt.php?id={}".format(book)
+        response_for_download = requests.get(url_for_download, allow_redirects=False)
+        response_for_download.raise_for_status() 
+        url_for_title = "http://tululu.org/b{}".format(book)
+        response_for_title = requests.get(url_for_title)
+        response_for_title.raise_for_status()
+        if response_for_title and response_for_download.content:
+            soup = BeautifulSoup(response_for_title.text, 'lxml')
+            image = soup.find("div", class_="bookimage").find("a").find("img")["src"]
+            image = urljoin(url_for_title, image)
+            response_image = requests.get(image)
+            filename = os.path.join(images_folder, "{0}{1}".format(book, image[-4:]))
+            with open(filename, 'wb') as file:
+                file.write(response_image.content)
+
+if __name__ == "__main__":             
+    download_image()
+
--- test_bs.py
+++ test_bs.py
@@ -1,14 +1,2 @@
-import requests
-from bs4 import BeautifulSoup
-
-
-url = "http://tululu.org/b1/"
-response = requests.get(url)
-response.raise_for_status()
-soup = BeautifulSoup(response.text, 'lxml')
-title = soup.find("h1")
-title = title.text.split("::")
-header = title[0].strip()
-author = title[1].strip()
-print("Заголовок:", header)
-print("Автор:", author)
+image = "alkator.jpg"
+print(image[-4:])
