3341b486dcf27c8953ebd50573ef6be7a6d78c6a
Alkator-Nori <nais000k@gmail.com>
2020-06-02 20:40:49 +0300

    get links from all pages

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -2,15 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
+for page in range(1, 11):
+	print("Страница №{}. Ссылки:".format(page))
+	url = "http://tululu.org/l55/{}".format(page)
+	response = requests.get(url)
+	response.raise_for_status()
+	soup = BeautifulSoup(response.text, "lxml")
+	all_books = soup.find_all("div", class_="bookimage")
 
-url = "http://tululu.org/l55"
-response = requests.get(url)
-response.raise_for_status()
-soup = BeautifulSoup(response.text, "lxml")
-all_books = soup.find_all("div", class_="bookimage")
-
-for book in all_books:
-	book = book.find("a")["href"]
-	book = urljoin(url, book)
-	print(book)
+	for book in all_books:
+		book = book.find("a")["href"]
+		book = urljoin(url, book)
+		print(book)
 
