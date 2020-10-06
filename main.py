7f5c9355eef773c836fa0fd53e9af65093df5e28
Alkator-Nori <nais000k@gmail.com>
2020-10-06 16:01:29 +0300

    Simplified code using enumerate

--- main.py
+++ main.py
@@ -29,8 +29,7 @@ def get_links_for_books(start_page, end_page):
     for page in range(start_page, end_page):
         url = "http://tululu.org/l55/{}".format(page)
         response = check_link(url)
-        soup = BeautifulSoup(response.text, "lxml")
-        all_books = soup.select(".bookimage")
+        all_books = BeautifulSoup(response.text, "lxml").select(".bookimage")
         for book in all_books:
             book = book.select_one("a")["href"]
             book = urljoin(url, book)
@@ -49,15 +48,13 @@ def download_books(links_for_books, json_path, books_folder="books"):
             header = title[0].strip()
             header = sanitize_filename(header)
             author = title[1].strip()
-            id_book = link.split(".org/b")
-            id_book = id_book[1]
+            id_book = link.split(".org/b")[1]
             image = soup.select_one(".bookimage a img")["src"]
             image = urljoin(link, image)
             filename = os.path.join(books_folder, header)
 
             #Добавляем жанр книги
-            genres = soup.find("span", class_="d_book")
-            genres = genres.text
+            genres = soup.find("span", class_="d_book").text
 
             #Добавляем комментарии
             comments = soup.select("div .texts")
@@ -86,15 +83,13 @@ def create_json(links_for_books, json_path, books_folder="books"):
             header = title[0].strip()
             header = sanitize_filename(header)
             author = title[1].strip()
-            id_book = link.split(".org/b")
-            id_book = id_book[1]
+            id_book = link.split(".org/b")[1]
             image = soup.select_one(".bookimage a img")["src"]
             image = urljoin(link, image)
             filename = os.path.join(books_folder, header)
 
             #Добавляем жанр книги
-            genres = soup.find("span", class_="d_book")
-            genres = genres.text
+            genres = soup.find("span", class_="d_book").text
 
             #Добавляем комментарии
             comments = soup.select("div .texts")
@@ -113,19 +108,16 @@ def create_json(links_for_books, json_path, books_folder="books"):
 
 
 def download_images(links_for_books, images_folder="images"):
-    number = 1
-    for link in links_for_books:
+    for link_number, link in enumerate(links_for_books):
         checked_link_for_title = check_link(link)
         if checked_link_for_title:
-            soup = BeautifulSoup(checked_link_for_title.text, 'lxml')
-            images = soup.select("div .bookimage")
+            images = BeautifulSoup(checked_link_for_title.text, 'lxml').select("div .bookimage")
             for image in images:
                 image_url = image.select_one("a img")["src"]
                 image_url = urljoin(link, image_url)
                 image_name = image_url.split("tululu.org/")
                 checked_link_for_image = check_link(image_url)
-                filename = os.path.join(images_folder, "{0}{1}".format(number, image_url[-4:]))
-                number += 1
+                filename = os.path.join(images_folder, "{0}{1}".format(link_number, image_url[-4:]))
                 with open(filename, 'wb') as file:
                     file.write(checked_link_for_image.content)
 
