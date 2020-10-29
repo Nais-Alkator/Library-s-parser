84cdd86806a11335c158240e59ebedf4f5a7d095
Alkator-Nori <nais000k@gmail.com>
2020-10-29 23:44:51 +0300

    Changes names of variables and functions, added tests, reduced the quantity of the network requests.

--- main.py
+++ main.py
@@ -8,115 +8,115 @@ import argparse
 import sys
 
 
-def check_link(url):
+def get_books_urls(start_page, end_page):
+    books_urls = []
+    for page in range(start_page, end_page):
+        url = "http://tululu.org/l55/{}".format(page)
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as error:
+            print("ОШИБКА HTTP ERROR: ", error)
+            sys.exit()
+        except requests.exceptions.ConnectionError as error:
+            print("ОШИБКА СОЕДИНЕНИЯ: ", error)
+            sys.exit()
+        books_info = BeautifulSoup(response.text, "lxml").select(".bookimage")
+        for book in books_info:
+            book_link = book.select_one("a")["href"]
+            book_url = urljoin(url, book_link)
+            books_urls.append(book_url)
+    return books_urls
+
+
+def parse_book_page(book_url):
     try:
-        checked_link = requests.get(url, allow_redirects=True)
-        checked_link.raise_for_status()
+        book_page = requests.get(book_url, allow_redirects=True)
+        book_page.raise_for_status()
     except requests.exceptions.HTTPError as error:
         print("ОШИБКА HTTP ERROR: ", error)
         sys.exit()
     except requests.exceptions.ConnectionError as error:
         print("ОШИБКА СОЕДИНЕНИЯ: ", error)
         sys.exit()
-    return checked_link
+    if book_page.content:
+        soup = BeautifulSoup(book_page.text, 'lxml')
+        title = soup.select_one("h1")
+        title = title.text.split("::")
+        header = title[0].strip()
+        header = sanitize_filename(header)
+        author = title[1].strip()
+        book_id = book_url.split(".org/b")[1]
+        image_url = soup.select_one(".bookimage a img")["src"]
+        image_url = urljoin(book_url, image_url)
+        book_path = os.path.join(books_folder, header)
+        genres = soup.find("span", class_="d_book").text
+        comments = soup.select("div .texts")
+        comments_info = []
+        for comment in comments:
+            comment = comment.select_one("span")
+            comments_info.append(comment.text)
+        parsed_book_page = {"title": header, "author": author, "image_url": image_url, "book_path": book_path, 
+        "comments": comments_info, "genres": genres, "book_id": book_id}
+    return parsed_book_page
 
 
-def get_links_for_books(start_page, end_page):
-    links_for_books = []
-    for page in range(start_page, end_page):
-        url = "http://tululu.org/l55/{}".format(page)
-        response = check_link(url)
-        all_books = BeautifulSoup(response.text, "lxml").select(".bookimage")
-        for book in all_books:
-            book = book.select_one("a")["href"]
-            book = urljoin(url, book)
-            links_for_books.append(book)
-    return links_for_books
+def fetch_parsed_books_pages(books_urls):
+    parsed_books_pages = []
+    for book_url in books_urls:
+        parsed_book_page = parse_book_page(book_url)
+        parsed_books_pages.append(parsed_book_page)
+    return parsed_books_pages
 
 
-def download_books(links_for_books, json_path, books_folder="books"):
-    json_data = []
-    for link in links_for_books:
-        response = check_link(link)
-        if response.content:
-            soup = BeautifulSoup(response.text, 'lxml')
-            title = soup.select_one("h1")
-            title = title.text.split("::")
-            header = title[0].strip()
-            header = sanitize_filename(header)
-            author = title[1].strip()
-            id_book = link.split(".org/b")[1]
-            image = soup.select_one(".bookimage a img")["src"]
-            image = urljoin(link, image)
-            filename = os.path.join(books_folder, header)
-
-            #Добавляем жанр книги
-            genres = soup.find("span", class_="d_book").text
-
-            #Добавляем комментарии
-            comments = soup.select("div .texts")
-            comments_info = []
-            for comment in comments:
-                comment = comment.select_one("span")
-                comments_info.append(comment.text)
-
-            payload = {"txt.php": "", "id": id_book}
-            link_for_download_book = "http://tululu.org"
-            response_for_download = requests.get(link_for_download_book, params=payload, allow_redirects=False)
-
-            #Скачивание книг
-            with open(filename, 'wb') as file:
-                file.write(response_for_download.content)
+def create_json_file(parsed_books_pages, json_path):
+    with open(json_path, 'w', encoding="utf8") as file:
+        json.dump(parsed_books_pages, file, ensure_ascii=False)
 
 
-def create_json(links_for_books, json_path, books_folder="books"):
-    json_data = []
-    for link in links_for_books:
-        response = check_link(link)
-        if response.content:
-            soup = BeautifulSoup(response.text, 'lxml')
-            title = soup.select_one("h1")
-            title = title.text.split("::")
-            header = title[0].strip()
-            header = sanitize_filename(header)
-            author = title[1].strip()
-            id_book = link.split(".org/b")[1]
-            image = soup.select_one(".bookimage a img")["src"]
-            image = urljoin(link, image)
-            filename = os.path.join(books_folder, header)
-
-            #Добавляем жанр книги
-            genres = soup.find("span", class_="d_book").text
-
-            #Добавляем комментарии
-            comments = soup.select("div .texts")
-            comments_info = []
-            for comment in comments:
-                comment = comment.select_one("span")
-                comments_info.append(comment.text)
+def download_book_file(book_id, book_path):
+    url = "http://tululu.org"
+    payload = {"txt.php": "", "id": book_id}
+    try:
+        book_file = requests.get(url, params=payload, allow_redirects=False)
+        book_file.raise_for_status()
+    except requests.exceptions.HTTPError as error:
+        print("ОШИБКА HTTP ERROR: ", error)
+        sys.exit()
+    except requests.exceptions.ConnectionError as error:
+        print("ОШИБКА СОЕДИНЕНИЯ: ", error)
+        sys.exit()
+    with open(book_path, 'wb') as file:
+        file.write(book_file.content)
 
 
-            info = {"title": header, "author": author, "img_src": image, "book_path": filename, 
-            "comments": comments_info, "genres": genres}
-            json_data.append(info)
-
-        with open(json_path, 'w', encoding="utf8") as file:
-            json.dump(json_data, file, ensure_ascii=False)
-
-
-def download_images(links_for_books, images_folder="images"):
-    for link_number, link in enumerate(links_for_books):
-        checked_link_for_title = check_link(link)
-        if checked_link_for_title:
-            images_tags = BeautifulSoup(checked_link_for_title.text, 'lxml').select("div .bookimage")
-            for image_tag in images_tags:
-                image_url = image_tag.select_one("a img")["src"]
-                image_url = urljoin(link, image_url)
-                image_name = image_url.split("tululu.org/")
-                checked_link_for_image = check_link(image_url)
-                filename = os.path.join(images_folder, "{0}{1}".format(link_number, image_url[-4:]))
-                with open(filename, 'wb') as file:
-                    file.write(checked_link_for_image.content)
+def download_image(image_url, images_folder, title):
+    try:
+        image_file = requests.get(image_url, allow_redirects=True)
+        image_file.raise_for_status()
+    except requests.exceptions.HTTPError as error:
+        print("ОШИБКА HTTP ERROR: ", error)
+        sys.exit()
+    except requests.exceptions.ConnectionError as error:
+        print("ОШИБКА СОЕДИНЕНИЯ: ", error)
+        sys.exit()
+    image_name = image_url.split("tululu.org/")
+    images_tags = BeautifulSoup(image_file.text, 'lxml').select("div .bookimage")
+    for image_tag in images_tags:
+        image_url = image_tag.select_one("a img")["src"]
+        image_url = urljoin(link, image_url)
+        image_name = image_url.split("tululu.org/")
+        try:
+            image_file = requests.get(image_url)
+        except requests.exceptions.HTTPError as error:
+            print("ОШИБКА HTTP ERROR: ", error)
+            sys.exit()
+        except requests.exceptions.ConnectionError as error:
+            print("ОШИБКА СОЕДИНЕНИЯ: ", error)
+            sys.exit()
+    filename = os.path.join(images_folder, "{0}{1}".format(title, image_url[-4:]))
+    with open(filename, 'wb') as file:
+        file.write(image_file.content)
 
 
 def get_parser():
@@ -138,26 +138,37 @@ if __name__ == "__main__":
     skip_imgs = args.skip_imgs
     skip_txt = args.skip_txt
     json_folder = args.json_path
-    books = os.path.join(dest_folder, "books")
-    images = os.path.join(dest_folder, "images")
+    books_folder = os.path.join(dest_folder, "books")
+    images_folder = os.path.join(dest_folder, "images")
     json_path = os.path.join(json_folder, "info.json")
-    os.makedirs(books, exist_ok=True)
-    os.makedirs(images, exist_ok=True)
+    os.makedirs(books_folder, exist_ok=True)
+    os.makedirs(images_folder, exist_ok=True)
     os.makedirs(json_folder, exist_ok=True)
-    links_for_books = get_links_for_books(start_page, end_page)
+    books_urls = get_books_urls(start_page, end_page)
+    parsed_books_pages = fetch_parsed_books_pages(books_urls)
     if skip_txt and skip_imgs:
-        create_json(links_for_books, json_path, books_folder=books)
-        print("Ничего не скачивается. Создан json файл.")
+        create_json_file(parsed_books_pages, json_path)
+        print("Ничего не скачивается. Создан json файл c информацией о книгах.")
     elif skip_imgs:
-        download_books(links_for_books, json_path, books_folder=books)
-        create_json(links_for_books, json_path, books_folder=books)
-        print("Скачивание книг завершено. Создан json файл.")
+        for parsed_book_page in parsed_books_pages:
+            title = parsed_book_page["title"]
+            book_path = parsed_book_page["book_path"]
+            download_book_file(title, book_path)
+        create_json_file(parsed_books_pages, json_path)
+        print("Скачивание книг завершено. Создан json файл с информацией о книгах.")
     elif skip_txt:
-        download_images(links_for_books, images_folder=images)
-        create_json(links_for_books, json_path, books_folder=books)
-        print("Скачивание обложек завершено. Создан json файл.")
+        for parsed_book_page in parsed_books_pages:
+            image_url = parsed_book_page["image_url"]
+            title = parsed_book_page["title"].split('/')[0]
+            download_image(image_url, images_folder, title)
+        create_json_file(parsed_books_pages, json_path)
+        print("Скачивание обложек завершено. Создан json файл с информацией о книгах.")
     else:
-        download_books(links_for_books, json_path, books_folder=books)
-        download_images(links_for_books, images_folder=images)
-        create_json(links_for_books, json_path, books_folder=books)
-        print("Скачивание книг и обложек завершено. Создан json файл.")
+        for parsed_book_page in parsed_books_pages:
+            image_url = parsed_book_page["image_url"]
+            title = parsed_book_page["title"].split('/')[0]
+            book_path = parsed_book_page["book_path"]
+            download_book_file(title, book_path)
+            download_image(image_url, images_folder, title)
+        create_json_file(parsed_books_pages, json_path)
+        print("Скачивание книг и обложек завершено. Создан json файл с информацией о книгах.")
